2677f3b4e9e69f21ac5e188a11106851ca58c585
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-07-13 09:01:13 -1000

    Added CORS middleware

--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const Model = require('./models/Post');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/post', (req, res) => {
   new Model.Post()
 		.fetchAll()
